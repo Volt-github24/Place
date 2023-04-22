4ac454004f550ca9bb56dc69084231da64b5e6dc
Voltaire Tchamba <parnelltchamba24@gmail.com>
2023-04-22 09:13:42 +0100

    ok

--- core/views.py
+++ core/views.py
@@ -56,9 +56,9 @@ class FacebookSocialAuthView(generics.GenericAPIView):
 # classe de pagination customisee
 class CustomPagination(PageNumberPagination):
 
-    page_size = 5
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 10
+    max_page_size = 20
 
     def get_paginated_response(self, data):
 
